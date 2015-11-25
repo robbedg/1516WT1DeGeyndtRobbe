0740cbf6ff2b5c7eba4cf6cb3d91e0b4edb3f587
robbe.degeyndt <robbe.degeyndt@student.odisee.be>
2015-11-25 23:08:26 +0100

    Project 1 25/11/15 23:07

---
+++ Project 1/Code/Project 1/DataImplementation/WriteWAVE.cs
@@ -0,0 +1,75 @@
+﻿using DataInterfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImplementation
+{
+    public class WriteWAVE : IWriteWAVE
+    {
+        private Dictionary<char, int> dict { get; set; }
+
+        public char[] textArray { get; set; }
+
+        public void readCSV()
+        {
+            dict = File.ReadLines("test.csv").Select(line => line.Split(',')).ToDictionary(line => line[0][0], line => Int32.Parse(line[1]));
+        }
+
+        public void toWAVE()
+        {
+
+            FileStream stream = new FileStream("test.wav", FileMode.Create);
+            BinaryWriter writer = new BinaryWriter(stream);
+
+            int RIFF = 0x46464952;
+            int WAVE = 0x45564157;
+            int formatChunkSize = 16;
+            int headerSize = 8;
+            int format = 0x20746D66;
+            short formatType = 1;
+            short tracks = 1;
+            int samplesPerSecond = 44100;
+            short bitsPerSample = 16;
+            short frameSize = (short)(tracks * ((bitsPerSample + 7) / 8));
+            int bytesPerSecond = samplesPerSecond * frameSize;
+            int waveSize = 4;
+            int data = 0x61746164;
+            int samples = 44100 * textArray.Count();
+            int dataChunkSize = samples * frameSize;
+            int fileSize = waveSize + headerSize + formatChunkSize + headerSize + dataChunkSize;
+
+            writer.Write(RIFF);
+            writer.Write(fileSize);
+            writer.Write(WAVE);
+            writer.Write(format);
+            writer.Write(formatChunkSize);
+            writer.Write(formatType);
+            writer.Write(tracks);
+            writer.Write(samplesPerSecond);
+            writer.Write(bytesPerSecond);
+            writer.Write(frameSize);
+            writer.Write(bitsPerSample);
+            writer.Write(data);
+            writer.Write(dataChunkSize);
+
+            double ampl = 10000;
+
+            for (int i = 0; i < textArray.Count(); i++)
+            {
+                int freq = dict[textArray[i]];
+                for (int j = 0; j < samples / textArray.Count(); j++)
+                {
+                    double t = (double)j / (double)samplesPerSecond;
+                    short s = (short)(ampl * (Math.Sin(t * freq * 2.0 * Math.PI)));
+                    writer.Write(s);
+                }
+            }
+            writer.Close();
+            stream.Close();
+        }
+    }
+}
---
+++ Project 1/Code/Project 1/GUI/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox = new System.Windows.Forms.RichTextBox();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(12, 12);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(260, 113);
+            this.TextBox.TabIndex = 1;
+            this.TextBox.Text = "";
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(12, 131);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(260, 40);
+            this.buttonSubmit.TabIndex = 2;
+            this.buttonSubmit.Text = "Submit";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.TextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TextBox;
+        private System.Windows.Forms.Button buttonSubmit;
+    }
+}
+
