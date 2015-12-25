c2b8790cf8ff904901aa2b936a0f7272fb0d3035
robbe.degeyndt <robbe.degeyndt@student.odisee.be>
2015-12-25 23:03:54 +0100

    Initial Commit (Project 2) 25/12/15 23:03

---
+++ Project 2/Code/Project 2/DataImplementation/ReadWAV.cs
@@ -0,0 +1,49 @@
+﻿using DataInterfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ReadWAV : IReadWAV
+    {
+        //https://stackoverflow.com/questions/8754111/how-to-read-the-data-in-a-wav-file-to-an-array
+        private float bytesToFloat(byte firstByte, byte secondByte)
+        {
+            //Put 2 bytes together.
+            short full = (short)(firstByte | ((int)secondByte) << 8);
+            //Return value as float.
+            return (float)(Convert.ToDouble(full));
+        }
+
+        public float[] getWav(string path)
+        {
+            //Read file
+            byte[] wav = File.ReadAllBytes(path);
+
+            //Get past all the header-chunks
+            int pos = 44;
+
+            //length of datachunk (16 bit => 2 bytes)
+            int length = (wav.Length - pos) / 2;
+
+            //make Array with de correct length
+            float[] sound = new float[length];
+
+            //Put data in float-array
+            int i = 0;
+            while (pos < length)
+            {
+                sound[i] = bytesToFloat(wav[pos], wav[pos + 1]);
+                pos += 2;
+                i++;
+            }
+
+            //return values
+            return sound;
+        }
+    }
+}
---
+++ Project 2/Code/Project 2/DataInterfaces/IReadWAV.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataInterfaces
+{
+    public interface IReadWAV
+    {
+        //Gets floatarray of data from WAV file.
+        float[] getWav(string path);
+    }
+}
